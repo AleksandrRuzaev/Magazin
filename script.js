d37c8d492a2b8d3951b8e8e422b274d23dd8c805
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2021-10-10 00:44:52 +0400

    Add basket modal

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 import './style.css';
 import { GoodsList, Basket } from './src/models/index';
 
-const list = new GoodsList('.goods-list');
 const basket = new Basket('.basket-list');
+const list = new GoodsList('.goods-list', basket.addItem.bind(basket));
+
+const modal = document.querySelector('.basket-modal');
+const basketBtn = document.querySelector('.cart-button');
+const closeBtn = document.querySelector('.close');
+
+basketBtn.onclick = function () {
+    modal.style.display = 'flex';
+};
+
+closeBtn.onclick = function () {
+    modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
 
 list.fetchGoods().then((_) => {
     list.render();
