cdc1364d33b0529addf659c2cf9d7913b31a8a0b
AleksandrRuzaev <alwaysgonnafly@gmail.com>
2021-10-17 22:00:01 +0400

    Remove old files

--- src/models/index.js
+++
@@ -1,4 +0,0 @@
-export { Basket } from './basket';
-export { BasketItem } from './basket-item';
-export { GoodsItem } from './goods-item';
-export { GoodsList } from './goods-list';
--- src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const API_URL =
-    'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
